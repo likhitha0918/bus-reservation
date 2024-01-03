0048e15d9043bc8a0bd0aa3bc2c3a9107551d66e
Likhitha R <89694994+likhitha0918@users.noreply.github.com>
2024-01-03 22:04:32 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -46,7 +46,7 @@ $(document).ready(function() {
             click: function() {
                 if (this.status() == 'available') {
                     //let's create a new <li> which we'll add to the cart items
-                    $('<li>' + this.data().category + ' Seat # ' + this.settings.label + ': <b>$' + this.data().price + '</b> <a href="#" class="cancel-cart-item">[cancel]</a></li>')
+                    $('<li>' + this.data().category + ' Seat # ' + this.settings.label + ': <b>₹' + this.data().price + '</b> <a href="#" class="cancel-cart-item">[cancel]</a></li>')
                         .attr('id', 'cart-item-' + this.settings.id)
                         .data('seatId', this.settings.id)
                         .appendTo($cart);
